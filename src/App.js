1f6c2e8b8fe50f0312f2f49a1f00f4b6a7ef0d2b
musicprogram <jahenao17@misena.edu.co>
2020-09-16 13:39:51 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import './App.css'
 
 
 function App() {
 
-  const uploadImage = () =>{
+  const [loading,setLoading] = useState(false);
+  const [image,setImage] = useState("");
 
+  const uploadImage = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'react-upload-init');
+    setLoading(true);
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/my-site-juan-test/image/upload",{
+      method: 'POST',
+      body:data
+    })
+
+    const file = await res.json()
+
+    // console.log(file);
+
+    setImage(file.secure_url);
+    setLoading(false);
   }
+
+
+  useEffect(()=>{
+     fetch("https://res.cloudinary.com/my-site-juan-test/image/upload")
+      .then((imgs)=>{
+        debugger
+        console.log(imgs)
+      })
+  },[])
+
 
   return (
     <div className="App">
@@ -16,6 +47,14 @@ function App() {
         placeholder="Upload Image"
         onChange={uploadImage}
       />
+      <br/>
+      {
+        loading?(
+          <img src="https://icon-library.com/images/loading-icon-animated-gif/loading-icon-animated-gif-14.jpg"/>
+        ):(
+          <img src={image}/>
+        )
+      }
     </div>
   );
 }
